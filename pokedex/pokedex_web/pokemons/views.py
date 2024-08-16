a1e491b3b4020d8773e02d172fef3ffa76059774
oantoniovinicius <devantoniovinicius@gmail.com>
2024-08-15 23:05:53 -0300

    image url from pokemons

--- pokedex/pokedex_web/pokemons/views.py
+++ pokedex/pokedex_web/pokemons/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 import requests
 import random
 
-def getPokemonInfo(pokemon_name):
-    api = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
+def getPokemonInfo(pokemon_id):
+    api = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}'
     res = requests.get(api)
 
     if res.status_code == 200:
         poke = res.json()
         
         abilities = [i['ability']['name'] for i in poke['abilities']]
-        forms = [i['name'] for i in poke['forms']]
+        #forms = [i['name'] for i in poke['forms']]
         type = [i['type']['name'] for i in poke['types']]
         stats = [f"{i['stat']['name']}: {i['base_stat']}" for i in poke['stats']]
         image_url = poke['sprites']['front_default']  # Captura a URL da imagem
 
         return {
-            'name': pokemon_name,
+            'name': poke['name'],
             'abilities': abilities,
-            'forms': forms,
+            #'forms': forms,
             'type': type,
             'stats': stats,
             'image_url': image_url,  # Adiciona a URL da imagem ao dicionário
@@ -37,13 +37,12 @@ def getAllPokemons():
         return []
 
 def pokemon_list(request):
-    allPokemons = getAllPokemons()
-    if allPokemons:
-        randomPokemons = random.sample(allPokemons, 6)  #get 5 random pokemons
-        pokemons = [getPokemonInfo(pokemon) for pokemon in randomPokemons]
-    else:
-        pokemons = []
-
+    pokemons = []
+    for pokemon_id in range (1,7):
+        pokemon_info = getPokemonInfo(pokemon_id)
+        if pokemon_info:
+            pokemons.append(pokemon_info)
+    
     context = {
         'pokemons': pokemons
     }
