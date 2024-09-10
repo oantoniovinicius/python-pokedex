61c3e886834649a1e97190272f9fed13da2ea977
oantoniovinicius <devantoniovinicius@gmail.com>
2024-09-10 13:47:54 -0300

    vercel adjustments

--- pokedex_web/pokedex_web/settings.py
+++ pokedex_web/pokedex_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@10k#9y6_#m*n=7@5b%waeudxr^+lydw9cdqn_sj^)l$2y)xp@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -126,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
